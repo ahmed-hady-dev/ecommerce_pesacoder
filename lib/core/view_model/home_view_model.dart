ac4a54ed0d3f910c4524e360d328414466c4df21
ahmed <ahmed.hady.dev@gmail.com>
2022-09-15 17:30:18 +0200

    bottom navigation bar logic added

--- lib/core/view_model/home_view_model.dart
+++ lib/core/view_model/home_view_model.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../view/cart_view.dart';
+import '../../view/home_view.dart';
+import '../../view/profile_view.dart';
 
 class HomeViewModel extends GetxController {
   int _navigatorValue = 0;
 
   int get navigatorValue => _navigatorValue;
+  List<Widget> viewsList = [
+    HomeView(),
+    CartView(),
+    ProfileView(),
+  ];
 
   void changeSelectedValue({required int selectedValue}) {
     _navigatorValue = selectedValue;
--- lib/view/control_view.dart
+++ lib/view/control_view.dart
@@ -1,4 +1,5 @@
-import 'package:ecommerce_pesacoder/view/home_view.dart';
+import 'package:ecommerce_pesacoder/core/view_model/home_view_model.dart';
+import 'package:ecommerce_pesacoder/view/widgets/home_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -12,7 +13,12 @@ class ControlView extends GetWidget<AuthViewModel> {
       if ((Get.find<AuthViewModel>().user == null)) {
         return LoginView();
       } else {
-        return HomeView();
+        return GetBuilder<HomeViewModel>(builder: (controller) {
+          return Scaffold(
+            bottomNavigationBar: HomeBottomNavigationBar(),
+            body: controller.viewsList[controller.navigatorValue],
+          );
+        });
       }
     });
   }
